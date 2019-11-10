b7f2bba603edffcb8df4e585b0d4a528b3e6b268
Guillermo <gpm0009@alu.ubu.es>
2019-11-10 19:51:14 +0100

    Añadir mapa y marcadores
    Se ha añadido un mapa de google maps a la aplicación web y es posible añadir nuevos marcadores en el mapa.

--- Metrominuto/app.py
+++ Metrominuto/app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_googlemaps import GoogleMaps
 
 from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET'])
 def pinta_mapa():
+    latitud=42.34
+    longitud=-3.69
     return render_template(
-        "map_template.html"
+        "map_template.html",
+        latitud=latitud,
+        longitud=longitud
     )
+
+if __name__ == '__main__':
+    app.run(debug=True)
