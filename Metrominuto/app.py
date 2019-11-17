ef8f86e653c73d139ddddb0953a16f5c5445247c
Guillermo <gpm0009@alu.ubu.es>
2019-11-17 19:23:54 +0100

    Mostrar los marcadores
    Se muestran en pantalla los marcadores guardados (desde JavaScript)

--- Metrominuto/app.py
+++ Metrominuto/app.py
@@ -1,29 +1,25 @@
 from flask import Flask, render_template, request
-#from flask_googlemaps import GoogleMaps
+#from datetime import datetime
 
-
-from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/",methods=['GET'])
+
+@app.route("/", methods=['GET'])
 def pinta_mapa():
-    latitud = 42.34
-    longitud = -3.69
+    latitude = 42.34
+    longitude = -3.69
     return render_template(
         "map_template.html",
-        latitud=latitud,
-        longitud=longitud
+        latitud=latitude,
+        longitud=longitude
     )
 
-@app.route("/markers")
+
 def save_markers():
-    _markers = request.args.get['markers']
-    print(_markers)
-    return  render_template(
-        "prueba.html",
-        marks=_markers,
-    )
+    print("save markers")
+    return 0
+
 
 if __name__ == '__main__':
     app.run(debug=True)
