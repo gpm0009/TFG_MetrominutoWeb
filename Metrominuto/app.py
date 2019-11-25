a43804807b618011d3f1744cb14e346ffb253c08
Guillermo <gpm0009@alu.ubu.es>
2019-11-25 22:16:23 +0100

    Guardar marcadores
    Varias pruebas con el fin de guardar las variables desde JS. Valorar como usar el api de google para dibujar las rutas y calcular las distancias.

--- Metrominuto/app.py
+++ Metrominuto/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, json
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map, icons
 #from datetime import datetime
 
 
@@ -9,20 +11,30 @@ app = Flask(__name__)
 def show_map():
     latitude = 42.34
     longitude = -3.69
-    return render_template(
-        "map_template.html",
-        latitud=latitude,
-        longitud=longitude
-    )
+    if request.method == 'GET':
+        return render_template(
+            "map_template.html",
+            latitud=latitude,
+            longitud=longitude
+        )
+    else:
+        markers = request.get_json();
+        print(markers[1])
+        return render_template("map_route.html",)
 
 
-@app.route("/getMarks", methods=['POST'])
-def get_marks():
-    markers = request.get_json();
-    print(markers)
-    return render_template('marks_show.html',
-                           markers=markers,)
+@app.route("/show_route", methods=['GET'])
+def show_route():
+
+    return 0
+
+
+# @app.route("/getMarks", methods=['POST'])
+# def get_marks():
+#     markers = request.args.list('data')
+#     #print(markers)
+#     return markers
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
