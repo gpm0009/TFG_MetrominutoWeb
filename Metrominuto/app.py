ba30311548c9e6b76b93ce1bafdfbd36f166144b
Guillermo <gpm0009@alu.ubu.es>
2019-12-18 19:15:00 +0100

    Leer datos útiles #29
    -Distance Matrix -> leer datos devueltos por el API en el servidor: read_matrix_distance(matrix_distance)
    -Directions -> leer datos devueltos por el API en el servidor: read_direction(directions)

--- Metrominuto/app.py
+++ Metrominuto/app.py
@@ -42,15 +42,56 @@ def set_marks():
     destinations.append(markers[2]['position'])
     now = datetime.now()
     # devuelven diccionarios
-    matrix_distance = gmaps.distance_matrix(origins, destinations)
+    origins_prueba = ["Bobcaygeon ON", [41.43206, -81.38992]]
+    destinations_prueba = [(43.012486, -83.6964149),
+                    {"lat": 42.8863855, "lng": -78.8781627}]
+
+    matrix = gmaps.distance_matrix(origins_prueba, destinations_prueba)
+
+
     directions_result = gmaps.directions(markers[0]['position'],
                                          markers[1]['position'],
                                          mode="transit",
                                          departure_time=now)
-    print(matrix_distance)
+    print(matrix)
     print(directions_result)
+    read_matrix_distance(matrix)
+    read_direction(directions_result)
     return render_template("map_template.html")
 
+
+#recibe un diccionario
+def read_matrix_distance(matrix_distance):
+    rows = matrix_distance['rows'] #Lista de disccionarios.
+    #matrix['rows'][0]['elements'][0]['distance']['value']
+    for row in rows:
+        elements = row['elements']
+        for element in elements:
+            distance = element['distance']['value']
+    # matrix['destination_addresses'][0]
+    destination_addresses = matrix_distance['destination_addresses'] #List
+    origin_addresses = matrix_distance['origin_addresses'] #List
+
+    return 0
+
+
+#recibe una lista con un diccionario
+def read_direction(directions):
+    trace = directions[0] #Diccionario
+    # directions_result[0]['warnings'][0]
+    #directions_result[0]['waypoint_order']
+    #directions_result[0]['legs'][0]['distance']['value']
+    #directions_result[0]['legs'][0]['end_location']['lat']
+    warnings = trace['warnings']
+    for warnin in warnings:
+        print(warnin)
+    legs = trace['legs']
+    for leg in legs:
+        distance = legs['distance']['value']
+        end_location_lat = leg['end_location']['lat']
+        end_location_lng = leg['end_location']['lng']
+
+    return 0
 
 if __name__ == '__main__':
     app.run()
