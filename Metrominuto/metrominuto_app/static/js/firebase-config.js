e8b10146fc0aafe421337035d4bad5d9ce4e84e3
gpm0009 <gpm0009@alu.ubu.es>
2020-06-25 12:01:51 +0200

    Ficheros conf

--- Metrominuto/metrominuto_app/static/js/firebase-config.js
+++ Metrominuto/metrominuto_app/static/js/firebase-config.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDJ_VkQXSRhz8g5DA2BoE6S-n951KHTp1c",
-    authDomain: "tfgguille.firebaseapp.com",
-    databaseURL: "https://tfgguille.firebaseio.com",
-    projectId: "tfgguille",
-    storageBucket: "tfgguille.appspot.com",
-    messagingSenderId: "621664518095",
-    appId: "1:621664518095:web:828ca09fb34b43e1a89cba"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
